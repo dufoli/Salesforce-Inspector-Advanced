6d1f58ba7672d8edd5df6e1e54767d2e0c1bcec0
olivier Dufour <olivier.duff@gmail.com>
2025-12-04 13:20:31 +0100

    fix field suggestion selection

--- addon/data-export.js
+++ addon/data-export.js
@@ -412,11 +412,7 @@ class Model {
     if (selStart != selEnd) {
       searchTerm = this.editor.value.substring(selStart, selEnd);
     } else if (this.autocompleteResults.isFieldValue) {
-      if (this.autocompleteResults.isFieldValue[2] != null) {
-        searchTerm = this.autocompleteResults.isFieldValue[2];
-      } else {
-        searchTerm = this.editor.value.substring(0, selEnd).match(/[^,(\s']*$/i)[0];
-      }
+      searchTerm = this.editor.value.substring(0, selEnd).match(/[^,=<>(\s']*$/i)[0];
       selStart -= searchTerm.length;
       //include open quote in selection
       if (this.editor.value.substring(0, selStart).endsWith("'")) {
